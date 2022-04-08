c08237396ab5ef20a5b95ac07f9c97c36660a550
M.Nassar <Nossa123##>
2022-04-08 21:34:29 +0200

    feat(main): Add function to remove site from Cloudflare

--- main.py
+++ main.py
@@ -6,11 +6,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script To Create New Cloudflare Zone and Configure DNS Records")
     parser.add_argument("-d", "--domain", type=str, help="Website domain name")
     parser.add_argument("-e", "--email", type=str, help="Account Email Used in Authentication")
-
     parser.add_argument("-k", "--api-key", type=str, help="The Global API Key")
-
     parser.add_argument("-a", "--ip", nargs="?", type=str, help="Website IP address")
-
     parser.add_argument("--delete", default=False, action="store_true", help="Delete the whole website")
     args = parser.parse_args()
 
